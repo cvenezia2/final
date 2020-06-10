733c4d5c8f6dfe06d99d167273b03e2d55a78371
cvenezia2 <cara.venezia@kellogg.northwestern.edu>
2020-06-10 05:01:31 +0000

    Commit with User Auth

--- app.rb
+++ app.rb
@@ -17,6 +17,12 @@ after { puts; }                                                                 
 
 libraries_table = DB.from(:libraries)
 reviews_table = DB.from(:reviews)
+users_table = DB.from(:users)
+
+before do
+    @current_user = users_table.where(:id => session[:user_id]).to_a[0]
+    puts @current_user.inspect
+end
 
 get "/" do 
     @libraries = libraries_table.all
@@ -25,11 +31,10 @@ get "/" do
 end
 
 get "/libraries/:id" do
+    @users_table = users_table
     @library = libraries_table.where(:id => params["id"]).to_a[0]
     @reviews = reviews_table.where(:library_id => params["id"]).to_a
     @count = reviews_table.where(:library_id => params["id"]).count
-    puts @library.inspect
-    puts @reviews.inspect
     view "library"
 end
 
@@ -39,14 +44,51 @@ get "/libraries/:id/reviews/new" do
     view "new_review"
 end
 
-get "/libraries/:id/reviews/create" do
-# Do stuff
-puts params.inspect
-reviews_table.insert(:library_id => params["id"],
-                    :review => params["review"],
-                    :name => params["name"],
-                    :email => params["email"],
-                    :booktitle => params["book title"],
-                    :comments => params["comments"])
-view "create_review"
+# Receiving end of new RSVP form
+post "/libraries/:id/reviews/create" do
+    reviews_table.insert(:library_id => params["id"],
+                       :review => params["review"],
+                       :user_id => @current_user[:id],
+                       :booktitle => params["book title"],
+                       :comments => params["comments"])
+    @library = libraries_table.where(:id => params["id"]).to_a[0]
+    view "create_review"
+end
+
+get "/users/new" do
+    view "new_user"
+end
+
+post "/users/create" do
+    users_table.insert(:name => params["name"],
+                       :email => params["email"],
+                       :password => params["password"])
+    view "create_user"
+end
+
+get "/logins/new" do
+    puts params
+    view "new_login"
+end
+
+post "/logins/create" do
+    puts params
+    email_entered = params["email"]
+    password_entered = params["password"]
+    user = users_table.where(:email => email_entered).to_a[0]
+    if user
+        puts user.inspect
+        if user[:password] == password_entered
+            session[:user_id] = user[:id]
+            view "create_login"
+        else
+            view "create_login_failed"
+        end
+    else 
+        view "create_login_failed"
+    end
+end
+
+get "/logout" do
+    view "logout"
 end
